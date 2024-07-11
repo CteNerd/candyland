308c8491c07764131abe8460c262471b54dd4b14
Roddy Tomlin <104323423+roddy-t@users.noreply.github.com>
2024-07-11 11:39:44 -0500

    Home Page :candy:
    add details about the game to the home page as well as a link to the game.

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,17 @@
+import './home.css';
+
 export default function Home() {
     return (
-        <div>
-            <h1>Home</h1>
+        <div className="container">
+            <h1>Candyland Game</h1>
+            <p>
+                This project was created to mimic Hasbro's CandyLand game for the purpose of demonstrating technical skills. 
+                Please note that this application is not intended to replace the actual game. 
+                It only includes the card drawing functionality and does not involve moving pieces across a game board.
+            </p>
+            <div className="button-container">
+                <button onClick={() => window.location.href = '/game'}>Go to Game</button>
+            </div>
         </div>
     );
 }
